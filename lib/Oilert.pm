bb5d138792f1cf1258e369e8b7f5e6de7d49a4fe
Luke Closs <luke@5thplane.com>
2011-06-28 12:26:42 -0700

    Add UI to add/remove notifications

--- lib/Oilert.pm
+++ lib/Oilert.pm
@@ -1,6 +1,8 @@
 package Oilert;
 use Dancer ':syntax';
 use YAML qw/LoadFile/;
+use Oilert::Redis;
+use Number::Phone;
 
 our $VERSION = '0.1';
 
@@ -9,7 +11,29 @@ get '/' => sub {
 
     template 'index', {
         ships => [ @{ $data->{Tanker} }, @{ $data->{Cargo} } ],
+        message => params->{message},
     };
 };
+
+post '/notify' => sub {
+    my $redis = Oilert::Redis->new;
+    my $message = "No action taken.";
+    if (my $start_num = get_number('signup_number')) {
+        $message = "Added notifications for $start_num";
+    }
+    elsif (my $stop_num = get_number('stop_number')) {
+        $message = "Stopped notifications for $stop_num";
+    }
+
+    forward '/', { message => $message }, { method => 'GET' };
+};
+
+sub get_number {
+    my $name = shift;
+    my $digits = "+1" . params->{$name};
+    my $num = Number::Phone->new($digits);
+    return $digits if $num and $num->is_valid;
+    return undef;
+}
 
 true;

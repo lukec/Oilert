0553af7f30937dd936aa48564267428e2ab58abe
Luke Closs <luke@5thplane.com>
2011-06-28 11:53:45 -0700

    Show basic status on the web

--- lib/Oilert.pm
+++ lib/Oilert.pm
@@ -1,10 +1,15 @@
 package Oilert;
 use Dancer ':syntax';
+use YAML qw/LoadFile/;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+    my $data = LoadFile("data/ships.yaml");
+
+    template 'index', {
+        ships => [ @{ $data->{Tanker} }, @{ $data->{Cargo} } ],
+    };
 };
 
 true;
